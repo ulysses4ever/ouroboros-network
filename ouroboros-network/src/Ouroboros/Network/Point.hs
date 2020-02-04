f2d4306ae40a93952919eb2d4e1cbf242d95261f
Nicolas Frisby <nick.frisby@iohk.io>
2020-02-03 21:37:44 -0800

    network: showWithoutRecordSyntax for Block

--- ouroboros-network/src/Ouroboros/Network/Point.hs
+++ ouroboros-network/src/Ouroboros/Network/Point.hs
@@ -18,14 +18,18 @@ module Ouroboros.Network.Point
 
 import           GHC.Generics (Generic)
 
-import           Cardano.Prelude (NoUnexpectedThunks)
+import           Cardano.Prelude (NoUnexpectedThunks, GShowK1 (..),
+                     gshowsPrecWithoutRecordSyntax)
 import           Cardano.Slotting.Slot
 
 data Block slot hash = Block
   { blockPointSlot :: !slot
   , blockPointHash :: !hash
   }
-  deriving (Eq, Ord, Show, Generic, NoUnexpectedThunks)
+  deriving (Eq, Ord, Generic, NoUnexpectedThunks)
+
+instance (Show hash, Show slot) => Show (Block slot hash) where
+  showsPrec = gshowsPrecWithoutRecordSyntax
 
 block :: slot -> hash -> WithOrigin (Block slot hash)
 block slot hash = at (Block slot hash)
